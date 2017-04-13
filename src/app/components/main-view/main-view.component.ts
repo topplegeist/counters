50132fba23d5e46ee1ebaf44494594e5056e0aba
Delsan <daniele.caldarola@gmail.com>
2017-04-13 15:00:05 +0200

    initialize main-view

---
+++ src/app/components/main-view/main-view.component.ts
@@ -0,0 +1,9 @@
+import {Component} from "@angular/core";
+/**
+ * Created by Soulbound Team on 13/04/17.
+ */
+@Component({
+  selector:'main-view',
+  templateUrl:'main-view.html'
+})
+export class MainViewComponent{}
