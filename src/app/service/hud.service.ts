f8cab174613bf574f7560ca4c5082d1a9c016c89
Ruj89 <Ruj89@users.noreply.github.com>
2019-03-19 14:54:26 +0100

    Added 5 points variable in hud service

--- src/app/service/hud.service.ts
+++ src/app/service/hud.service.ts
@@ -7,4 +7,5 @@ import {Injectable} from "@angular/core";
 @Injectable()
 export class HUDService {
   public showIcons: boolean[] = [true, true];
+  public activate5PointsButtons: boolean[] = [false, false],
 }
